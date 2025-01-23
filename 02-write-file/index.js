85c58303c879ae8d2fc53ea6e2d68da471f25047
Iryna Serhiienko <irishka.sergienko@gmail.com>
2025-01-23 23:31:13 +0200

    feat: add a message when exiting by Ctrl+C.

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const readline = require('readline');
+
+const { stdin, stdout } = process;
 
 // Create a path to the file where we will write the text
 const filePath = path.join(__dirname, 'output.txt');
@@ -8,31 +9,26 @@ const filePath = path.join(__dirname, 'output.txt');
 // Create a stream of writing to the file
 const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
 
-// Create an interface for reading input from the console
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-console.log(
-  'Welcome! Enter the text you want to write to the file. To exit, enter "exit" or press Ctrl+C.',
+stdout.write(
+  'Welcome! Enter the text you want to write to the file. To exit, enter "exit" or press Ctrl+C.:\n',
 );
 
 // Listen to user input
-rl.on('line', (input) => {
-  if (input.trim().toLocaleLowerCase() === 'exit') {
-    console.log('Thank you! The work is complete.');
-    rl.close();
+stdin.on('data', (data) => {
+  const inputData = data.toString().trim().toLowerCase();
+  if (inputData === 'exit') {
+    stdout.write('\nThank you! The work is complete.');
+    process.exit();
   } else {
     // Write the entered text to a file
-    writeStream.write(`${input}\n`);
-    console.log(
-      'The text is recorded. Enter the following text or "exit" to exit.',
+    writeStream.write(`${data}\n`);
+    stdout.write(
+      'The text is recorded. Enter the following text or "exit" to exit.\n',
     );
   }
 });
 
 process.on('SIGINT', () => {
-  console.log('\nFarewell! The work is completed.');
-  rl.close();
+  stdout.write('\nFarewell! The work is completed.');
+  process.exit();
 });
